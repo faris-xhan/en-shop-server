b7a6fb829d34e97c119e2d816fbf558a7ef58c21
faris <faris@mail.com>
2021-12-23 19:09:12 +0500

    :sparkles: create register router

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const { User } = require("../models");
+
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  return res.json({ msg: "Working on auth route" });
+router.post("/register", async (req, res, next) => {
+  try {
+    const user = new User(req.body);
+    const savedUser = await user.save();
+    return res.json(savedUser);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
