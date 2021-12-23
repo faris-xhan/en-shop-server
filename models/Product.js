dcf907861693618ca480c70ac6536070e25dfe44
faris <faris@mail.com>
2021-12-23 18:32:46 +0500

    :fire: add timestamp to product model

--- models/Product.js
+++ models/Product.js
@@ -1,14 +1,17 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  title: { type: String, require: true, unique: true },
-  price: { type: Number, require: true },
-  desc: { type: String, require: true },
-  img: { type: String, require: true },
-  categories: { type: Array },
-  color: { type: String },
-  size: { type: String },
-});
+const productSchema = new mongoose.Schema(
+  {
+    title: { type: String, require: true, unique: true },
+    price: { type: Number, require: true },
+    desc: { type: String, require: true },
+    img: { type: String, require: true },
+    categories: { type: Array },
+    color: { type: String },
+    size: { type: String },
+  },
+  { timestamps: true }
+);
 
 const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
