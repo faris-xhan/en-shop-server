293e312dd59fda52b598757eb17d875842bfe750
faris <faris@mail.com>
2021-12-23 18:29:24 +0500

    :fire: update product model

--- models/Product.js
+++ models/Product.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
   title: { type: String, require: true, unique: true },
-  categories: { type: Array, require: true },
-  price: { type: String, require: true },
-  color: { type: String, require: true },
+  price: { type: Number, require: true },
   desc: { type: String, require: true },
-  size: { type: String, require: true },
   img: { type: String, require: true },
+  categories: { type: Array },
+  color: { type: String },
+  size: { type: String },
 });
 
 const Product = mongoose.model("Product", productSchema);
