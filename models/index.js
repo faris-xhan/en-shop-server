88968e9619efa8cffaabb90ae10fd84b90ecde9d
faris <faris@mail.com>
2021-12-23 18:43:23 +0500

    :recyle: create index file in models to import easily

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const Cart = require("./Cart");
+const Order = require("./Order");
+const Product = require("./Product");
+const User = require("./User");
+
+const Models = {
+  Cart,
+  Order,
+  Product,
+  User,
+};
+
+module.exports = Models;
