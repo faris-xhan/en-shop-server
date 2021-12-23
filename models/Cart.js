43375e63497c10b20ccff0891132573b76779f32
faris <faris@mail.com>
2021-12-23 18:37:05 +0500

    :sparkles: add cart model

---
+++ models/Cart.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const product = {
+  product: { type: String },
+  quantity: { type: Number, default: 1 },
+};
+
+const cartSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    products: [product],
+  },
+  { timestamps: true }
+);
+
+const Cart = mongoose.model("Cart", cartSchema);
+module.exports = Cart;
