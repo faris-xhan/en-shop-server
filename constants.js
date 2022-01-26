ed4e791bbe16ea4ef5b8edca5ec84fc97bc41bcf
faris <faris@mail.com>
2022-01-26 18:06:22 +0500

    :wrench: add stripe key

--- constants.js
+++ constants.js
@@ -5,4 +5,5 @@ const env = process.env;
 module.exports = {
   mongodbStr: env.MONGODB || 'mongodb://localhost:27017/test',
   jwtSecretKey: env.JWT_SECRET_KEY || 'somesecret',
+  stripeKey: env.STRIPE_KEY,
 };
